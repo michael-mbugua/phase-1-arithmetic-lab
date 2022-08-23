b8ead096e85a1b848cb0d0b2e9825b8bc057551d
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-08-23 09:57:16 +0300

    completed arithmetic operations lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+let num1=31;
+let num2=2;
+let multiply=num1 * num2;
+console.log(multiply)
+let random=Math.floor(Math.random() * 10) + 4;
+console.log(random)
+let num3=22;
+let num4=6;
+let mod=num3 %num4;
+console.log(mod)
